2ffb3665704e89d0163bd7edfb120e6b5da00d50
Shahram Anver <shahram@datacue.co>
2020-10-30 01:14:51 +0800

    removed pwa references

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router/index";
 import store from "./store/index";
 import DataCueDashboard from "./plugins/datacueDashboard";
-import "./registerServiceWorker";
+// import "./registerServiceWorker";
 
 /* eslint-disable no-new */
 createApp(App)
