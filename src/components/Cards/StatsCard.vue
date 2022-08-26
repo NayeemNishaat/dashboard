6648eed772c39e4ffb0694c0fbb338f5fa9951bb
lAbYriNth <RxNayeemIMI@gmail.com>
2022-08-26 14:46:24 +0600

    fixed icon issue

--- src/components/Cards/StatsCard.vue
+++ src/components/Cards/StatsCard.vue
@@ -72,34 +72,34 @@ export default {
           parseISO(this.dateRange?.[0])
         ) + 1
       );
-    },
+    }
   },
   props: {
     loading: {
       type: Boolean,
-      default: false,
+      default: false
     },
     title: {
       type: String,
-      default: "",
+      default: ""
     },
     value: {
       type: String,
-      default: "",
+      default: ""
     },
     comparison: {
       type: String,
-      default: "",
+      default: ""
     },
     icon: {
       type: String,
-      default: "",
-    },
+      default: ""
+    }
   },
   components: {
     Card,
-    LoaderDots,
-  },
+    LoaderDots
+  }
 };
 </script>
 
