b4fec1e2da50c6793ec28853ad3706926ec9fd9b
nayeemnishaat <rxnayeemimi@gmail.com>
2022-08-30 11:23:27 +0600

    writing tests

--- tests/unit/vitest/summary.spec.js
+++ tests/unit/vitest/summary.spec.js
@@ -1,14 +1,15 @@
-// import mutations from "@/store/mutations";
-// // import { mockAuthenticatedClients, webSettings } from "./mocks/store";
+import mutations from "@/store/mutations";
+import { assert, describe, it, expect } from "vitest";
+// import { mockAuthenticatedClients, webSettings } from "./mocks/store";
 
-// it("Sets a date range!", () => {
-//   const newRange = ["2020-07-15", "2020-04-15"];
-//   const state = {
-//     dateRange: ["2020-05-07", "2020-05-15"]
-//   };
+it("Sets a date range!", () => {
+  const newRange = ["2020-07-15", "2020-08-03"];
+  const state = {
+    dateRange: ["2020-05-07", "2020-05-15"]
+  };
 
-//   mutations.setDateRange(state, newRange);
-//   expect(state).toEqual({
-//     dateRange: newRange
-//   });
-// });
+  mutations.setDateRange(state, newRange);
+  expect(state).toEqual({
+    dateRange: newRange
+  });
+});
