1674264f22bb0f5f722ff70b9bd5f1972f150951
nayeemnishaat <rxnayeemimi@gmail.com>
2022-08-30 10:47:28 +0600

    writing tests

---
+++ tests/unit/vitest/summary.spec.js
@@ -0,0 +1,14 @@
+// import mutations from "@/store/mutations";
+// // import { mockAuthenticatedClients, webSettings } from "./mocks/store";
+
+// it("Sets a date range!", () => {
+//   const newRange = ["2020-07-15", "2020-04-15"];
+//   const state = {
+//     dateRange: ["2020-05-07", "2020-05-15"]
+//   };
+
+//   mutations.setDateRange(state, newRange);
+//   expect(state).toEqual({
+//     dateRange: newRange
+//   });
+// });
