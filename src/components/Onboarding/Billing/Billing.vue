e26f28b3e1dda9ee66b1a9f90f38e7257efadd42
nayeemnishaat <rxnayeemimi@gmail.com>
2022-09-08 09:15:12 +0600

    writing tests

--- src/components/Onboarding/Billing/Billing.vue
+++ src/components/Onboarding/Billing/Billing.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="flex flex-col md:flex-row">
     <div
-      class="md:w-[53%] 2xl:w-[50%] max-w-full flex items-center md:block flex-col relative bg-[#D5EFDA] rounded-[50px] md:rounded-none md:rounded-tr-[120px] lg:rounded-tr-[200px] ring-[14px] ring-inset ring-white px-6 sm:px-8 lg:!px-12 !pt-5 pb-36 md:!py-5">
+      class="md:w-[53%] 2xl:w-[50%] max-w-full flex items-center md:block flex-col relative bg-[#D5EFDA] rounded-[50px] md:rounded-none md:rounded-tr-[120px] lg:rounded-tr-[200px] ring-[14px] ring-inset ring-white px-6 sm:px-8 lg:!px-12 !pt-5 pb-36 md:!py-5"
+    >
       <a href="#" class="inline-block xl:pl-20 !mt-5 md:mt-0">
         <figure>
           <img src="@/assets/img/logo.svg" alt="logo" />
         </figure>
       </a>
-      <BillingLeft v-if="!planStore.loading" @ChangePlan="ChangePlan" :plans="planStore.plans" :plan="currentPlan" />
+      <BillingLeft
+        v-if="!planStore.loading"
+        @ChangePlan="ChangePlan"
+        :plans="planStore.plans"
+        :plan="currentPlan"
+      />
       <LoaderDots v-else />
     </div>
     <div
-      class="px-8 pt-10 md:h-full pb-6 relative -mt-[6rem] md:mt-0 md:pl-3 lg:pl-5 xl:pl-5 md:w-[750px] max-w-full rounded-tl-[100px] md:rounded-none bg-white 2xl:pl-24 md:py-8 md:pr-6">
+      class="px-8 pt-10 md:h-full pb-6 relative -mt-[6rem] md:mt-0 md:pl-3 lg:pl-5 xl:pl-5 md:w-[750px] max-w-full rounded-tl-[100px] md:rounded-none bg-white 2xl:pl-24 md:py-8 md:pr-6"
+    >
       <Bullet :store="store" />
       <BillingRight v-if="!planStore.loading" :plan="currentPlan" />
-      <Control @previous="previous" @next="next" :store="store" :nextLoading="planStore.finishing"
-        :nextDisabled="!currentPlan.plan_id" />
+      <Control
+        @previous="previous"
+        @next="next"
+        :store="store"
+        :nextLoading="planStore.finishing"
+        :nextDisabled="!currentPlan.plan_id"
+      />
     </div>
   </div>
 </template>
@@ -25,7 +37,7 @@ import Control from "@/components/Onboarding/Control.vue";
 import Bullet from "@/components/Onboarding/Bullet.vue";
 import BillingLeft from "@/components/Onboarding/Billing/BillingLeft.vue";
 import BillingRight from "@/components/Onboarding/Billing/BillingRight.vue";
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 import {
   selectPlan,
@@ -40,14 +52,13 @@ import { OnboardingStore } from "../interfaces";
 
 const props = defineProps({
   store: Object as PropType<OnboardingStore>
-}
-);
+});
 const emit = defineEmits(["previous", "next"]);
 const { t } = useI18n({});
 const planStore = reactive({
   plans: [] as Array<DisplayPlan>,
   loading: true,
-  finishing: false,
+  finishing: false
 });
 const router = useRouter();
 const mainStore = useStore();
@@ -61,17 +72,23 @@ const finishOnboarding = async () => {
   if (hasFinishedOnboarding) {
     await router.push({ name: "setup-summary" });
   }
-}
+};
 const ChangePlan = (plan_id) => {
   planStore.plans = planStore.plans.map((el) => {
     return {
       ...el,
-      selected: el.plan_id == plan_id ? true : false,
+      selected: el.plan_id == plan_id ? true : false
     };
   });
 };
-const currentPlan = computed(() => planStore.plans.find((plan) => plan.selected === true) || {} as DisplayPlan);
-const hasFinishedOnboarding = computed(() => mainStore.getters["onboarding/hasFinishedOnboarding"]);
+const currentPlan = computed(
+  () =>
+    planStore.plans.find((plan) => plan.selected === true) ||
+    ({} as DisplayPlan)
+);
+const hasFinishedOnboarding = computed(
+  () => mainStore.getters["onboarding/hasFinishedOnboarding"]
+);
 const previous = () => {
   emit("previous");
 };
@@ -91,22 +108,23 @@ onMounted(async () => {
   if (res?.subscriptions?.active?.active) {
     await finishOnboarding();
     planStore.loading = false;
-    return
+    return;
   }
 
   planStore.plans = res.available_plans
     .filter((plan) => plan.active)
     .map((plan) => {
-      let displayPlan: DisplayPlan = Object.assign(plan, { features: [] as Array<string>, selected: false });
+      let displayPlan: DisplayPlan = Object.assign(plan, {
+        features: [] as Array<string>,
+        selected: false
+      });
       displayPlan = fillPlanDetails(displayPlan, t);
       return displayPlan;
     })
     .sort((lhs, rhs) => lhs.price - rhs.price);
   planStore.plans[0].selected = true;
   planStore.loading = false;
-})
-
+});
 </script>
 
-<style>
-</style>
+<style></style>
